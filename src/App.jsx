083d47645912eddb44b205f321c78ffcef4b42e5
Camran1234 <Camran750@gmail.com>
2024-02-04 15:00:43 -0600

    [ADD] Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
+import githubLogo from './assets/github.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
+import {format} from 'date-fns'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+  const [formatDate, setFormatDate] = useState('dd/mm/yyyy');
+  useEffect(() => {
+    // Actualizar la fecha y hora cada segundo
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+      setFormatDate(format(new Date(), 'dd/MM/yyyy'));
+    }, 1000);
+    
+    // Limpieza del intervalo al desmontar el componente
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href='https://github.com/Camran1234/SA-P1.git' target='_blank'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href='https://github.com/Camran1234/SA-P1.git' target='_blank'>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href='https://github.com/Camran1234/SA-P1.git' target='_blank'>
+          <img src={githubLogo} className="logo github" alt="Github logo" />
+        </a>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Jonathan Marcos Valiente González</h1>
+      <h2>201931581</h2>
+      <div>
+        <h3>{formatDate}</h3>
+        <p>{currentDateTime.toLocaleTimeString()}</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        App hecha con Vite y template de React
       </p>
     </>
   )
