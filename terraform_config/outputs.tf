800f5d210734b61a03b5ad42f6fb75e8ce47886f
Camran1234 <Camran750@gmail.com>
2024-02-07 19:10:47 -0600

    [ADD] terraform code, [ADD] ansible code, [ADD] nginx deployment

--- terraform_config/outputs.tf
+++ terraform_config/outputs.tf
@@ -2,14 +2,10 @@ output "domain-name-master"{
     value=aws_instance.master.public_dns
 }
 
-output "application-url-master"{
-    value = "${aws_instance.master.public_dns}/index.php"
-}
-
 output "domain-name-client"{
     value=aws_instance.client.public_dns
 }
 
 output "application-url-client"{
-    value = "${aws_instance.client.public_dns}/index.php"
+    value = "${aws_instance.client.public_dns}/"
 }
